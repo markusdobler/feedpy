adf9b9785ad64723621380b71fe5268384e86849
Markus Dobler <mail@markusdobler.de>
2014-01-28 22:57:20 +0100

    several bug fixes

--- feedpy.py
+++ feedpy.py
@@ -45,7 +45,7 @@ class Feedpy(object):
         subscriptions = self._subscriptions()
         counts_list = self._get('/markers/counts')
         counts_list = [feed for feed in counts_list['unreadcounts'] if
-                       feed['id'].startswith('feed')]
+                       feed['id'].startswith('feed') and feed['count']]
         counts = defaultdict(lambda : [0, []])
         uncategorized = [dict(label='uncategorized',
                              id=self.api.global_resource_id('category', 'uncategorized'))]
@@ -69,28 +69,38 @@ class Feedpy(object):
             'unreadOnly': unread_only,
             'continuation': continuation,
         }
-        return self._get('/streams/contents', params)
+        content = self._get('/streams/contents', params)
+        self._add_keepunread_indicator(content['items'])
+        return content
 
-    def _post_to_markers(self, action, type, ids, last_entry_id=None):
+    def _add_keepunread_indicator(self, items):
+        saved_tag = self.api.global_resource_id('tag', 'saved')
+        for entry in items:
+            keep_unread = saved_tag in [t['id'] for t in entry.get('tags', ())]
+            entry['keepUnread'] = keep_unread
+
+
+    def _post_to_markers(self, action, type_singular, type_plural, ids, last_entry_id=None):
         data = {
             "action": action,
-            "type": type,
-            "entryIds": ids,
-            "lastReadEntryId": last_entry_id,
+            "type": type_plural,
+            "%sIds"%type_singular: ids,
         }
-        self._post('/markers', data)
+        if last_entry_id:
+            data['lastReadEntryId'] = last_entry_id
+        return self._post('/markers', data)
 
     def mark_articles_as_read(self, entry_ids):
-        self._post_to_markers('markAsRead', 'entries', entry_ids)
+        return self._post_to_markers('markAsRead', 'entry', 'entries', entry_ids)
 
     def mark_articles_as_unread(self, entry_ids):
-        self._post_to_markers('keepUnread', 'entries', entry_ids)
+        return self._post_to_markers('keepUnread', 'entry', 'entries', entry_ids)
 
     def mark_feed_as_read(self, feed_id, last_read_entry):
-        self._post_to_markers('markAsRead', 'feeds', [feed_id], last_read_entry)
+        return self._post_to_markers('markAsRead', 'feed', 'feeds', [feed_id], last_read_entry)
 
     def mark_category_as_read(self, category_id, last_read_entry):
-        self._post_to_markers('markAsRead', 'categories', [category_id], last_read_entry)
+        return self._post_to_markers('markAsRead', 'category', 'categories', [category_id], last_read_entry)
 
     def recently_read(self, **params):
         stream = self.api.global_resource_id('tag', 'read')
@@ -107,8 +117,10 @@ class FeedlyAPIRequestException(FeedlyAPIException):
 
 def retry_with_authorization_renewed(request):
     def wrapper(self, *args, **kwargs):
+        print request, args
         r = request(self, *args, **kwargs)
         if r.status_code == 401:
+            print "retry"
             self.reauthenticate()
             r = request(self, *args, **kwargs)
         if r.status_code not in (200,):
@@ -125,7 +137,7 @@ class FeedlyAPI(object):
     # This should be overwritten by derived classes that have other endpoints
     base_url = 'http://sandbox.feedly.com/v3'
     client_id = 'sandbox'
-    client_secret = 'FUFNPXDNP2J0BF7RCEUZ'
+    client_secret = 'CM786L1D4P3M9VYUPOB8'
     redirect_uri = 'http://localhost'
 
 
@@ -170,14 +182,14 @@ class FeedlyAPI(object):
         # extract code from url if necessary
         r = re.search('[?&]code=([-_a-zA-Z0-9]*)(&.*)?$', code_or_url)
         code = r.group(1) if r else code_or_url
-        params = {
+        data = {
             'code': code,
             'client_id': cls.client_id,
             'client_secret': cls.client_secret,
             'redirect_uri': cls.redirect_uri,
             'grant_type': 'authorization_code',
         }
-        r = requests.post(cls.url('/auth/token'), params=params)
+        r = requests.post(cls._full_url('/auth/token'), data=data).json()
         return r['id'], r['refresh_token'], r['access_token']
 
 
@@ -188,8 +200,10 @@ class FeedlyAPI(object):
             'client_secret': self.client_secret,
             'grant_type': 'refresh_token',
         }
-        r = self.post('/auth/token', params)
-        self._set_authorization(r['access_token'])
+        r = self.post_no_reauthenticate('/auth/token', params)
+        if r.status_code != 200:
+            raise FeedlyAPIException("Couldn't reauthenticate: " + r.text)
+        self._set_authorization(r.json()['access_token'])
 
     def _set_authorization(self, access_token):
         self._session.headers.update({'Authorization': 'OAuth %s'%access_token})
@@ -210,6 +224,9 @@ class FeedlyAPI(object):
 
     @retry_with_authorization_renewed
     def post(self, path, data=None):
+        return self.post_no_reauthenticate(path, data)
+
+    def post_no_reauthenticate(self, path, data=None):
         """ Low-level post access to API
 
         req = api.post('/markers', {
@@ -221,10 +238,25 @@ class FeedlyAPI(object):
         data = json.dumps(data) if data else '{}'
         return self._session.post(self._full_url(path), data=data)
 
+    @retry_with_authorization_renewed
+    def put(self, path, data=None):
+        """ Low-level put access to API"""
+        data = json.dumps(data) if data else '{}'
+        return self._session.put(self._full_url(path), data=data)
+
+    @retry_with_authorization_renewed
+    def delete(self, path, data=None):
+        """ Low-level delete access to API"""
+        data = json.dumps(data) if data else '{}'
+        return self._session.delete(self._full_url(path), data=data)
+
     @classmethod
     def _full_url(cls, path):
         """ Combine bae_url and path """
         return cls.base_url + path
 
     def global_resource_id(self, kind, name):
-        return 'user/%s/%s/global.%s' % (self._user_id, kind, name)
+        return self.resource_id(kind, 'global.%s'%name)
+
+    def resource_id(self, kind, name):
+        return 'user/%s/%s/%s' % (self._user_id, kind, name)
